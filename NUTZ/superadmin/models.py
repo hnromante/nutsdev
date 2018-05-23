1d925c60d320922ffea5e2d3fc003cf06491f880
MrChikibell <f.campana@live.com>
2018-05-22 22:47:51 -0400

    carga base datos

--- NUTZ/superadmin/models.py
+++ NUTZ/superadmin/models.py
@@ -10,22 +10,25 @@ class SuperAdmin(models.Model):
 
 class GrupoAlimento(models.Model):
     nombre = models.CharField(max_length=50)
-    cho_prom = models.IntegerField()
-    pro_prom = models.IntegerField()
-    lip_prom = models.IntegerField()
-    azu_prom = models.IntegerField()
+    kcal_prom = models.FloatField()
+    cho_prom = models.FloatField()
+    pro_prom = models.FloatField()
+    lip_prom = models.FloatField()
+    azu_prom = models.FloatField()
+   
 
     def __str__(self):
         return self.nombre
 
 class Alimento(models.Model):
     nombre = models.CharField(max_length=50)
-    cho = models.IntegerField()
-    pro = models.IntegerField()
-    lip = models.IntegerField()
-    azu = models.IntegerField()
+    kcal = models.FloatField()
+    cho = models.FloatField()
+    pro = models.FloatField()
+    lip = models.FloatField()
+    azu = models.FloatField()
     grupo_alimento = models.ForeignKey(GrupoAlimento, on_delete=models.CASCADE)
-    porcion = models.IntegerField(default=1)
+    porcion = models.FloatField(default=1)
 
     def __str__(self):
         return self.nombre
