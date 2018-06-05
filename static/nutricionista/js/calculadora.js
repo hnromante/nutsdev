4aa3853e03e5611fbb04f177db84a623af973b72
hnromante <hugonunez@thedogcompany.cl>
2018-06-05 19:49:25 -0400

    event handller cambiado

--- static/nutricionista/js/calculadora.js
+++ static/nutricionista/js/calculadora.js
@@ -26,59 +26,60 @@ $.getJSON(url_alimentos_json, function(data){
 /**
 *!Función JQuery que captura la porcion y los atributos
 */
-$(document).ready(function(){ 
-    $(".calculadora-body tr input").on("keyup change", function(){
-        console.log("KEY UP CHANGE")
-        $(".calculadora-body").children('tr').each((i, e) => { 
-            const porcion = $(e).find("input").val()
-            const kcal = $(e).data("kcal")
-            const cho = $(e).data("cho")
-            const pro = $(e).data("pro")
-            const lip = $(e).data("lip")
-            atributos = [kcal, cho, pro, lip]
-            a_x_p = multiplicarAtributos(porcion, atributos)
-            //a_x_p = Atributos kcal,cho,pro,lip POR porcion.
-            $(e).find(".kcal").html(a_x_p[0])
-            $(e).find(".cho").html(a_x_p[1])
-            $(e).find(".pro").html(a_x_p[2])
-            $(e).find(".lip").html(a_x_p[3])    
+$(document).on("keyup change", "input", function(){
+    console.log("KEY UP CHANGE")
+    $(".calculadora-body").children('tr').each((i, e) => { 
+        const porcion = $(e).find("input").val()
+        const kcal = $(e).data("kcal")
+        const cho = $(e).data("cho")
+        const pro = $(e).data("pro")
+        const lip = $(e).data("lip")
+        atributos = [kcal, cho, pro, lip]
+        a_x_p = multiplicarAtributos(porcion, atributos)
+        //a_x_p = Atributos kcal,cho,pro,lip POR porcion.
+        $(e).find(".kcal").html(a_x_p[0])
+        $(e).find(".cho").html(a_x_p[1])
+        $(e).find(".pro").html(a_x_p[2])
+        $(e).find(".lip").html(a_x_p[3])    
 
-        })
-        //Asignar subtotal
-        const porciones = []
-        $(".porcion").each((i, e)=> { porciones.push($(e).find("input").val() )})
-        porciones_num = porciones.map(e => Number(e))
-
-        const kcals = []
-        $(".kcal").each((i,e) => { kcals.push($(e).html()) })
-        kcals_num = kcals.map(e => Number(e))
-
-        const chos = []
-        $(".cho").each((i,e) => { chos.push($(e).html()) })
-        chos_num = chos.map(e => Number(e))
-
-        const prots = []
-        $(".pro").each((i,e) => { prots.push($(e).html()) })
-        prots_num = prots.map(e => Number(e))
-
-        const lips = []
-        $(".lip").each((i,e) => { lips.push($(e).html()) })
-        lips_num = lips.map(e => Number(e))
-
-        sub_porciones = porciones_num.reduce((suma, val) => suma + val)
-        sub_kcals = kcals_num.reduce((suma, val) => suma + val)
-        sub_cho = chos_num.reduce((suma, val) => suma + val)
-        sub_pro = prots_num.reduce((suma, val) => suma + val)
-        sub_lip = lips_num.reduce((suma, val) => suma + val)
-
-        $(".sub-porcion").html(sub_porciones)
-        $(".sub-kcal").html(sub_kcals)
-        $(".sub-cho").html(sub_cho)
-        $(".sub-pro").html(sub_pro)
-        $(".sub-lip").html(sub_lip)
-        actualizarAdecuacion()
-        
     })
+    //Asignar subtotal
+    const porciones = []
+    $(".porcion").each((i, e)=> { porciones.push($(e).find("input").val() )})
+    porciones_num = porciones.map(e => Number(e))
+
+    const kcals = []
+    $(".kcal").each((i,e) => { kcals.push($(e).html()) })
+    kcals_num = kcals.map(e => Number(e))
+
+    const chos = []
+    $(".cho").each((i,e) => { chos.push($(e).html()) })
+    chos_num = chos.map(e => Number(e))
+
+    const prots = []
+    $(".pro").each((i,e) => { prots.push($(e).html()) })
+    prots_num = prots.map(e => Number(e))
+
+    const lips = []
+    $(".lip").each((i,e) => { lips.push($(e).html()) })
+    lips_num = lips.map(e => Number(e))
+
+    sub_porciones = porciones_num.reduce((suma, val) => suma + val)
+    sub_kcals = kcals_num.reduce((suma, val) => suma + val)
+    sub_cho = chos_num.reduce((suma, val) => suma + val)
+    sub_pro = prots_num.reduce((suma, val) => suma + val)
+    sub_lip = lips_num.reduce((suma, val) => suma + val)
+
+    $(".sub-porcion").html(sub_porciones)
+    $(".sub-kcal").html(sub_kcals)
+    $(".sub-cho").html(sub_cho)
+    $(".sub-pro").html(sub_pro)
+    $(".sub-lip").html(sub_lip)
+    actualizarAdecuacion()
+    
+})
+$(document).ready(function(){ 
+    
     total_kcal = $(".total-kcal").html()
     $(".kcal-cho").html(total_kcal)
     $(".gr-cho").html(total_kcal)
@@ -89,7 +90,6 @@ $(document).ready(function(){
     $(".kcal-lip").html(total_kcal)
     $(".gr-lip").html(total_kcal)
 
-    
 })
 
 /**
